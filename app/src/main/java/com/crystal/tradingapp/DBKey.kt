591e74ddcc3c5f76836461eda652294ff2c56597
kwonsoojeong <0401tnwjd@gmail.com>
2022-01-04 17:04:23 +0900

    DB 로드해서 Home fragment recycler view에  뿌리기

---
+++ app/src/main/java/com/crystal/tradingapp/DBKey.kt
@@ -0,0 +1,7 @@
+package com.crystal.tradingapp
+
+class DBKey {
+    companion object{
+        const val DB_ARTICLES = "Articles"
+    }
+}
--- app/src/main/java/com/crystal/tradingapp/home/HomeFragment.kt
+++ app/src/main/java/com/crystal/tradingapp/home/HomeFragment.kt
@@ -4,27 +4,56 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.crystal.tradingapp.DBKey
 import com.crystal.tradingapp.R
 import com.crystal.tradingapp.databinding.FragmentHomeBinding
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+
+
+    private lateinit var articleDB: DatabaseReference
+    private lateinit var articleAdapter: ArticleAdapter
+    private val articleList = mutableListOf<ArticleModel>()
+
+    private val listener = object: ChildEventListener{
+        override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+            //모델클래스 자체를 업로드한다. 따라서 클래스로 받는다
+            val articleModel = snapshot.getValue(ArticleModel::class.java)
+            articleModel ?: return
+            articleList.add(articleModel)
+            articleAdapter.submitList(articleList)
+        }
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+        }
+        override fun onChildRemoved(snapshot: DataSnapshot) {
+        }
+        override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+        }
+        override fun onCancelled(error: DatabaseError) {
+        }
+
+    }
     private var binding: FragmentHomeBinding? = null
-    private lateinit var articalAdapter: ArticleAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val fragmentHomeBinding= FragmentHomeBinding.bind(view)
         binding = fragmentHomeBinding
-        articalAdapter = ArticleAdapter()
-        articalAdapter.submitList(mutableListOf<ArticleModel>().apply {
-            add(ArticleModel("001","가방", 100000, "5000원",""))
-            add(ArticleModel("002","신발", 100001, "4000원",""))
-        })
+
+        articleList.clear()
+        articleDB = Firebase.database.reference.child(DBKey.DB_ARTICLES)
+        articleAdapter = ArticleAdapter()
+
 
         fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
-        fragmentHomeBinding.articleRecyclerView.adapter =articalAdapter
-
-
+        fragmentHomeBinding.articleRecyclerView.adapter =articleAdapter
+        articleDB.addChildEventListener(listener)
     }
 }
